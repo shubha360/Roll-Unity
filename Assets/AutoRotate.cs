5754aa45423ff3d95132c61fddfd5d5c544ab9af
Raquibul Islam <shubha360@outlook.com>
2021-12-01 12:44:46 +0600

    First commit

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    public Vector3 rotationChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationChange);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Roll-Unity
 This is my submission to Essentials 3D Project Gallery.
+
+You can [PLAY IT HERE](https://play.unity.com/mg/other/webgl-builds-120980).
+
+![thumbnail](Thumbnail.jpg)
